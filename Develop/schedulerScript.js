e91e5b8e792e352dff673dcc27d1ebdd8b9ebce1
Sinae Kim <54830877+shine7653@users.noreply.github.com>
2019-10-16 17:32:13 -0700

    one table made

---
+++ Develop/schedulerScript.js
@@ -0,0 +1,47 @@
+/* 
+var emailInput = document.querySelector("#email");        
+var userEmailSpan = document.querySelector("#user-email");
+var signUpButton = document.querySelector("#sign-up
+
+
+ function renderLastRegistered(
+     var aa = localStorage.getItem("email");
+     userEmailSpan.textContent = aa;
+     // console.log(aa);
+ }
+
+  signUpButton.addEventListener("click", 
+      function (event) {
+          event.preventDefault();
+          var aa = emailInput.value;
+          // console.log(aa);
+          /* if (aa === "") {
+             
+          } */
+      /*     localStorage.setItem("email", aa);
+          renderLastRegistered();
+      });
+ */ 
+
+$(document).ready(function(){
+
+  $("#empty-div").html("Hello world");
+  var arrayTime = $(input-group mb-3);
+  
+  var timeOption = ["9AM","10AM","11AM","12AM","1PM","2PM","3PM","4PM","5PM"];
+
+  for (var i = 0; i < timeOption.length; i++){
+    console.log('sinae');
+
+    
+
+
+
+
+
+
+
+  }
+
+});
+
