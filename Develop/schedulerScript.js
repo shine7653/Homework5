e7c5b8857608f8315a8aa7c7c8bd9cad4ec0f7e5
Sinae Kim <54830877+shine7653@users.noreply.github.com>
2019-10-18 20:55:28 -0700

    rows updated

--- Develop/schedulerScript.js
+++ Develop/schedulerScript.js
@@ -1,75 +1,74 @@
 
 $(document).ready(function(){
-  
-  $("#currentDay").html("Hello world~~~~~~~");
 
-/*   var current = $("moment().format('MMMM Do YYYY, h:mm:ss a')");
-  $("#currentDay").append(current);  */
-
-  console.log("screwbge");
+  const currentDate = moment().format("dddd,MMMM Do");
+  $("#currentDay").html(currentDate);
 
   var timeOption = ["9AM","10AM","11AM","12AM","1PM","2PM","3PM","4PM","5PM"];  
-  var currentTime = 0;
-/*     var question = document.createElement("h1");
-    //console.log(questions);
-    question.textContent = questions[quesIndex].title;
-    arrayQues.appendChild(question);
-  
-    // Set the choices
-    for (var i = 0; i < questions[quesIndex].choices.length ; i++) {
-      var button = document.createElement("button");
-      button.textContent = questions[quesIndex].choices[i];
-      arrayQues.appendChild(button); */
-  
-/* }); */
+  // var currentTime = 0;
 
-// a. function to build scheduler Rows (function buildRows) --> loop, moment.js
-function buildRows(firstRow) {
-  // build HTML elements row
-  // TODO - finish  
-
-      var time = $("<div>");
-      time.attr("data-timeOption", timeOption[0]);
-      time.text(timeOption[0]);
-      $("#aa").append(time);
-
-    /*   var scribble = $("<div>").html();
-      scribble.attr(textarea, "<textarea/>");
-      $("#bb").append(scribble); */
-
-      var scribble = $("<textarea/>");
-      $("#bb").append(scribble); 
-
-      var button = $("<button>");
-      $("#cc").append(button);  
-}
+  // a. function to build scheduler Rows (function buildRows) --> loop, moment.js
+  function buildRows(firstRow) {
+    // build HTML elements row
 
     for (var i = 0; i < timeOption.length; i++){
-      var x = $(fisrtRow);
-      $(".row").append(x);
-} 
 
-// b. Set date at top of page (function showTodaysDate) --> moment.js
-function showTodaysDate() {
-  // TODO - finish
-}
+        var row = $("<div class='row'>");
 
-// c. Save button event handler (function saveRow) --> uses localstorage
-function saveRow() {
-  // TODO - finish
+        var time = $("<div>");
+        time.attr("data-timeOption", timeOption[i]);
+        time.text(timeOption[i]);
+        // time.addclass(timeClass);
+        var timeSpan = $("<span>");
+        timeSpan.append(time);
+        // $("#aa").append(time);
 
+      /*   var scribble = $("<div>").html();
+        scribble.attr(textarea, "<textarea/>");
+        $("#bb").append(scribble); */
 
-}
+        var scribble = $("<textarea/>");
+        var userInput = $("<span>");
+        userInput.append(scribble);
+        // $("#bb").append(scribble); 
 
-// d. Change row styles (function updateRowStyle) --> moment.js
-function updateRowStyle() {
-  // TODO - finish
-  // i. on page load, check current time (hour) against rows in scheduler
-  // ii. update style for those that are past hours, present hour, future hours
-}
+        var button = $("<button>");
+        var saveBtn = $("<span>");
+        saveBtn.append(button);
+        // $("#cc").append(button); 
+        row.append(timeSpan,userInput,saveBtn);
+        $(".container").append(row);
+  }
+  }
+
+  // b. Set date at top of page (function showTodaysDate) --> moment.js
+  function showTodaysDate() {
+
+  }
+
+  // c. Save button event handler (function saveRow) --> uses localstorage
+  function saveRow() {
+    console.log(acea);
+    /*   $("saveBtn").click(
+        function(){
+
+        var contents = $("userInput").val();
+        localStorage.setItem("setValue", JSON.stringify(contents));
+
+        var value = JSON.parse(localStorage.getItem("setValue"));        
+        // userInput.innerHTML = value;
+      }) */
+  }
+
+  // d. Change row styles (function updateRowStyle) --> moment.js
+  function updateRowStyle() {
+
+    // i. on page load, check current time (hour) against rows in scheduler
+    // ii. update style for those that are past hours, present hour, future hours
+  }
 
 buildRows();
-
+// saveRow();
 
 /* 
 var emailInput = document.querySelector("#email");        
@@ -81,7 +80,7 @@ var signUpButton = document.querySelector("#sign-up
      var aa = localStorage.getItem("email");
      userEmailSpan.textContent = aa;
      // console.log(aa);
- }
+ }s
 
   signUpButton.addEventListener("click", 
       function (event) {
@@ -97,3 +96,9 @@ var signUpButton = document.querySelector("#sign-up
  */ 
 
 });
+
+
+// const key = inputKey.value;
+// localStorage.setItem(key);
+// const value = localStorage.getItem(key);
+// lsOutput.innerHTML = '${key}: ${value}<br />'  ;
